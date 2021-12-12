8a3232fa5c185a16911b9304c2e0315bd98deea2
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-12-12 00:28:57 -0500

    Lista de Inmuebles en Home

--- src/app/public/general/home/home.component.ts
+++ src/app/public/general/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigurationData } from 'src/app/config/ConfigurationData';
+import { InmuebleModel } from 'src/app/models/parameters/inmueble.model';
+import { InmuebleService } from 'src/app/services/parameters/inmueble.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  inmuebleList: InmuebleModel[] = [];
+  url: string = ConfigurationData.BUSSINESS_MS_URL;
+  constructor(private inmuebleService: InmuebleService) {}
 
   ngOnInit(): void {
+    this.GetInmuebleList();
   }
 
+  GetInmuebleList() {
+    this.inmuebleService.GetRecordList().subscribe({
+      next: (data: InmuebleModel[]) => {
+        this.inmuebleList = data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
